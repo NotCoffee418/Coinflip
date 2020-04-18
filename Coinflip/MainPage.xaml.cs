fb453e343de3ef98a610c1ce933bc2dc005b4ce0
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-04-18 05:19:41 +0200

    Vungle Ad
    Most important part of this useless test project :D
    Good for reference though.

--- Coinflip/MainPage.xaml.cs
+++ Coinflip/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using VungleSDK;
+using Windows.UI.Core;
+using Windows.ApplicationModel.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,34 @@ namespace Coinflip
         public MainPage()
         {
             this.InitializeComponent();
+            InitVungleAd();
+        }
+
+        VungleAd sdkInstance = null;
+
+        public void InitVungleAd()
+        {
+            // Prepare SDK instance
+            VungleSDKConfig sdkConfig = new VungleSDKConfig();
+            sdkConfig.DisableBannerRefresh = true; // Default: false
+            sdkInstance = AdFactory.GetInstance("5e9a5bb33f4c15000110043f", sdkConfig);
+
+            // Prepare control
+            vungleBottomBanner.AppID = "5e9a5bb33f4c15000110043f";
+            vungleBottomBanner.Placement = "BOTTOM-8749976";
+            vungleBottomBanner.Width = 728;
+            vungleBottomBanner.Height = 90;
+
+            // Event to load after init
+            sdkInstance.OnInitCompleted += SdkInstance_OnInitCompleted;
+        }
+
+        private async void SdkInstance_OnInitCompleted(object sender, ConfigEventArgs e)
+        {
+            await CoreApplication.MainView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                  new DispatchedHandler(() =>
+                    vungleBottomBanner.LoadAndPlayBannerAd("BOTTOM-8749976", VungleBannerSizes.BannerLeaderboard_728x90)
+                  ));
         }
     }
 }
